3690345f2342540742c9f1cb0e16f2ed0daf0197
Tim Ohlsen <tim.ohlsen2@gmail.com>
2022-04-16 01:28:06 +0200

    set default value for editor js if available

--- assets/js/components/Editor/Editor.js
+++ assets/js/components/Editor/Editor.js
@@ -17,7 +17,7 @@ export default function Editor({defaultValue, onChange}) {
 
     return (
         <div className="sm:col-span-8">
-            <ReactEditorJS holder='custom' tools={EDITOR_JS_TOOLS} onInitialize={handleInit} onChange={handleChange}>
+            <ReactEditorJS holder='custom' tools={EDITOR_JS_TOOLS} onInitialize={handleInit} onChange={handleChange} defaultValue={defaultValue}>
                 <div
                     id='custom'
                     className="w-full shadow-sm focus:ring-yellow-500 focus:border-yellow-500 block sm:text-sm border border-gray-300 rounded-md p-2"
